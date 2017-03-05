a6923cb87fbf16f80c40d1512bc761b77aeb17e3
Kennith Leung <kennith.leung@gmail.com>
2017-03-05 07:53:47 -0800

    add sample

--- adapter/adapter.php
+++ adapter/adapter.php
@@ -37,19 +37,55 @@ class Civic implements Car {
 	{
 		return 'turn on the key';
 	}
-	
+
 	function lock() 
 	{
 		return 'press the lock key on remote.';
 	}
 }
 
-class CivicAdapter {
-	private $civic;
+class CivicAdapter implements ElectricCar {
+	private $car;
 
-	function __construct(Civic $civic) 
+	function __construct(Car $car) 
 	{
-		$this->civic = $civic;
+		$this->car = $car;
+	}
+
+	function turnOn() 
+	{
+		return $this->car->start();
+	}
+
+	function parked() 
+	{
+		return $this->car->lock();
+	}
+
+}
+
+class Tesla implements ElectricCar {
+	function turnOn() {
+		return 'press a button.';
+	}
+
+	function parked() 
+	{
+		return 'Tesla will find a parking itself.';
+	}
+
+}
+
+class Person {
+	private $ec;
+	function __construct(ElectricCar $ec)
+	{
+		$this->ec = $ec;
+	}
+
+	function getOffCar()
+	{
+		return $this->ec->parked();
 	}
 }
 
@@ -58,8 +94,16 @@ interface Car {
 	function lock();
 }
 
+interface ElectricCar {
+	function turnOn();
+	function parked();
+}
+
 $t = new NameAdapter(new NameGenerator('kennith', 'leung'));
 echo $t->getName();
 
-$car = new Civic;
-echo $car->start();
+$person = (new Person(new Tesla));
+echo $person->getOffCar();
+
+$person = (new Person(new CivicAdapter(new Civic)));
+echo $person->getOffCar();
